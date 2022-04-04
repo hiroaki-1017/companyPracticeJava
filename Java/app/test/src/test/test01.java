7b9863d5cbe113c1f672a02e90d5303c201ce397
山田 寛顕 <hiroaki.yamada@salto.link>
2022-04-04 11:33:53 +0900

    fc

---
+++ Java/app/test/src/test/test01.java
@@ -0,0 +1,13 @@
+package test;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(1+1);
+		System.out.print("1 + 1");
+		System.out.print("1"+"1");
+		System.out.print("1"+1);
+	}
+
+}
